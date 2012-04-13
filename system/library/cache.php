48edde56005b9d43f51bbf0d2c1bf1b3a6dac7f2
Vadim A. Misbakh-Soloviov <mva@mva.name>
2012-04-13 17:57:19 +0700

    return of memcache support

--- system/library/cache.php
+++ system/library/cache.php
@@ -1,55 +1,105 @@
 <?php
-class Cache { 
-	private $expire = 3600; 
+final class Cache {
+	private $expire;
+	private $memcache;
+	private $ismemcache = false;
 
-  	public function __construct() {
-		$files = glob(DIR_CACHE . 'cache.*');
-		
-		if ($files) {
-			foreach ($files as $file) {
-				$time = substr(strrchr($file, '.'), 1);
+  	public function __construct($exp = 3600) {
+  		$this->expire = $exp;
 
-      			if ($time < time()) {
-					if (file_exists($file)) {
-						unlink($file);
-					}
-      			}
-    		}
+  		if (CACHE_DRIVER == 'memcached')
+  		{
+		    $mc = new Memcache;
+		    if ($mc->pconnect(MEMCACHE_HOSTNAME, MEMCACHE_PORT))
+		    {
+			$this->memcache = $mc;
+			$this->ismemcache = true;
+		    };
+		};
+
+		if (!$this->ismemcache)
+		{
+		    $files = glob(DIR_CACHE . 'cache.*');
+
+		    if ($files) {
+			    foreach ($files as $file) {
+				    $time = substr(strrchr($file, '.'), 1);
+
+			    if ($time < time()) {
+					    if (file_exists($file)) {
+						    @unlink($file);
+					    }
+				}
+			    }
+		    }
 		}
   	}
 
 	public function get($key) {
-		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', $key) . '.*');
+	    if ((CACHE_DRIVER == 'memcached') && $this->ismemcache)
+	    {
+		return($this->memcache->get(MEMCACHE_NAMESPACE . $key, 0));
+	    }
+	    else
+	    {
+		$files = glob(DIR_CACHE . 'cache.' . $key . '.*');
 
 		if ($files) {
-			$cache = file_get_contents($files[0]);
-			
-			return unserialize($cache);
+    		foreach ($files as $file) {
+      			$cache = '';
+
+				$handle = fopen($file, 'r');
+
+				if ($handle) {
+					$cache = fread($handle, filesize($file));
+
+					fclose($handle);
+				}
+
+	      		return unserialize($cache);
+   		 	}
 		}
-	}
+	    }
+  	}
 
   	public function set($key, $value) {
-    	$this->delete($key);
-		
-		$file = DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', $key) . '.' . (time() + $this->expire);
-    	
-		$handle = fopen($file, 'w');
+	    if ((CACHE_DRIVER == 'memcached') && $this->ismemcache)
+	    {
+		$this->memcache->set(MEMCACHE_NAMESPACE . $key, $value, 0, $this->expire);
+	    }
+	    else
+	    {
 
-    	fwrite($handle, serialize($value));
-		
-    	fclose($handle);
+    		    $this->delete($key);
+
+		    $file = DIR_CACHE . 'cache.' . $key . '.' . (time() + $this->expire);
+
+		    $handle = fopen($file, 'w');
+
+    		    fwrite($handle, serialize($value));
+
+    		    fclose($handle);
+    	    };
   	}
-	
+
   	public function delete($key) {
-		$files = glob(DIR_CACHE . 'cache.' . preg_replace('/[^A-Z0-9\._-]/i', '', $key) . '.*');
-		
+	    if ((CACHE_DRIVER == 'memcached') && $this->ismemcache)
+	    {
+		$this->memcache->delete(MEMCACHE_NAMESPACE . $key);
+	    }
+	    else
+	    {
+		$files = glob(DIR_CACHE . 'cache.' . $key . '.*');
+
 		if ($files) {
     		foreach ($files as $file) {
       			if (file_exists($file)) {
-					unlink($file);
+					@unlink($file);
+					clearstatcache();
 				}
     		}
 		}
+	    }
   	}
 }
 ?>
