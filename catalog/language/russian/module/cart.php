4fd97132f11cf6ab8f8b347105f76517e52ef1dd
Vadim A. Misbakh-Soloviov <mva@mva.name>
2012-05-07 23:31:20 +0700

    TF: Cart translation
    Signed-off-by: Vadim A. Misbakh-Soloviov <mva@mva.name>

--- catalog/language/russian/module/cart.php
+++ catalog/language/russian/module/cart.php
@@ -1,9 +1,9 @@
 <?php
-// Heading 
-$_['heading_title'] = 'Корзина покупок';
+// Heading
+$_['heading_title'] = 'Корзина';
 
 // Text
-$_['text_items']    = 'Товаров: %s (%s)';
+$_['text_items']    = 'В корзине: %s (%s)';
 $_['text_empty']    = 'Ничего не куплено!';
 $_['text_cart']     = 'Просмотр корзины';
 $_['text_checkout'] = 'Оформление заказа';
