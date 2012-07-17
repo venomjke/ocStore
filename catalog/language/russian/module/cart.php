621a5a60dfa256ac2c6dee6c8061799112b9ff03
Vadim A. Misbakh-Soloviov <mva@mva.name>
2012-07-18 06:38:38 +0700

    Isn't it better?

--- catalog/language/russian/module/cart.php
+++ catalog/language/russian/module/cart.php
@@ -3,7 +3,7 @@
 $_['heading_title'] = 'Корзина';
 
 // Text
-$_['text_items']    = 'В корзине:<br />%s (%s)';
+$_['text_items']    = '<nobr>Товаров: %s</nobr><br /><nobr>(%s)</nobr>';
 $_['text_empty']    = 'Ничего не куплено!';
 $_['text_cart']     = 'Просмотр корзины';
 $_['text_checkout'] = 'Оформление заказа';
