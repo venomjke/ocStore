f640063336881d429602f39ed629edc9db1287a4
Vadim A. Misbakh-Soloviov <mva@mva.name>
2012-07-29 10:34:44 +0700

    some fixes

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -88,8 +88,13 @@ class ControllerCommonHeader extends Controller {
 		}
 
 		// Menu
-		$this->load->model('catalog/category');
+		if (isset($this->request->get['path'])) {
+			$parts = explode('_', (string)$this->request->get['path']);
+		} else {
+			$parts = array();
+		}
 
+		$this->load->model('catalog/category');
 		$this->load->model('catalog/product');
 
 		$this->data['categories'] = array();
@@ -120,6 +125,7 @@ class ControllerCommonHeader extends Controller {
 				$this->data['categories'][] = array(
 					'name'     => $category['name'],
 					'children' => $children_data,
+					'active'   => in_array($category['category_id'], $parts),
 					'column'   => $category['column'] ? $category['column'] : 1,
 					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
 				);
