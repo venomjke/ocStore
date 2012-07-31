5ea2dd4f19f4c9ac04f2011588d1fc78e15272aa
Vadim A. Misbakh-Soloviov <mva@mva.name>
2012-07-31 23:44:03 +0700

    bump to current opencart state

---
+++ catalog/language/english/shipping/auspost.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_title']    = 'Australia Post';
+$_['text_express']  = 'Australia Post Express';
+$_['text_standard'] = 'Australia Post Standard';
+$_['text_eta']      = 'Estimated Time:';
+
+// Error
+$_['error_postcode'] = 'Your postcode is not valid in Australia';
+?>
