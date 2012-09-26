6b15294a3a061c4247adce14da2f63fc14c2841f
alex.strigin <apstrigin@gmail.com>
2012-09-26 05:27:15 +0400

    - обновление ядра - улучшил интерфейсы загрузки  модели,либы,хелпера -

--- system/engine/loader.php
+++ system/engine/loader.php
@@ -1,11 +1,31 @@
 <?php
 final class Loader {
+
 	protected $registry;
 
+	/**
+	 * Список загруженных библиотек
+	 *
+	 * @var array
+	 **/
+	private $_oc_classes = array();
+	/**
+	 * Список загруженных моделей
+	 *
+	 * @var array
+	 **/
+	private $_oc_models = array();
+	/**
+	 * Список загруженных хелперов
+	 *
+	 * @var array
+	 **/
+	private $_oc_helpers = array();
+	
 	public function __construct($registry) {
 		$this->registry = $registry;
 	}
-
+	
 	public function __get($key) {
 		return $this->registry->get($key);
 	}
@@ -13,63 +33,123 @@ final class Loader {
 	public function __set($key, $value) {
 		$this->registry->set($key, $value);
 	}
-
+	
 	public function library($library) {
-		$file = DIR_SYSTEM . 'library/' . $library . '.php';
 
+		/*
+		* Загрузка списка библиотек
+		*/
+		if(is_array($library)){
+			foreach($library as $lib){
+				$this->library($lib);
+			}
+			return FALSE;
+		}
+
+		/*
+		* Если либу уже загружали, то выходим отсюда
+		*/
+		if(in_array($library,$this->_oc_classes,true)){
+			return FALSE;
+		}
+
+		$file = DIR_SYSTEM . 'library/' . $library . '.php';
+		
 		if (file_exists($file)) {
-			include_once($file);
+			include_once($file);	
+			$this->_oc_classes[] = $library;
 		} else {
 			trigger_error('Error: Could not load library ' . $library . '!');
-			exit();
+			exit();					
 		}
 	}
-
+	
 	public function helper($helper) {
-		$file = DIR_SYSTEM . 'helper/' . $helper . '.php';
 
+		/*
+		* Загрузки списка хелперов
+		*/
+		if(is_array($helper)){
+			foreach($helper as $h){
+				$this->helper($h);
+			}
+			return FALSE;
+		}
+
+		/*
+		* Если хелпер уже ранее загружали, то ничего не делаем
+		*/
+		if(in_array($helper,$this->_oc_helpers,true)){
+			return FALSE;
+		}
+
+		$file = DIR_SYSTEM . 'helper/' . $helper . '.php';
 		if (file_exists($file)) {
 			include_once($file);
+			$this->_oc_helpers[] = $helper;
 		} else {
 			trigger_error('Error: Could not load helper ' . $helper . '!');
-			exit();
+			exit();					
 		}
 	}
-
+		
 	public function model($model) {
+
+		/*
+		* Загрузка стопки моделей
+		*/
+		if(is_array($model)){
+			foreach($model as $m){
+				$this->model($m);
+			}
+			return FALSE;
+		}
+		/*
+		* Если модель уже ранее загружали, то ничего не делаем.
+		*/
+		if(in_array($model,$this->_oc_models,true)){
+			return FALSE;
+		}
+
 		$file  = DIR_APPLICATION . 'model/' . $model . '.php';
 		$class = 'Model' . preg_replace('/[^a-zA-Z0-9]/', '', $model);
-
 		if (file_exists($file)) {
 			include_once($file);
-
+			
 			$this->registry->set('model_' . str_replace('/', '_', $model), new $class($this->registry));
+
+			$this->_oc_models[] = $model;
 		} else {
 			trigger_error('Error: Could not load model ' . $model . '!');
-			exit();
+			exit();					
 		}
 	}
-
+	 
 	public function database($driver, $hostname, $username, $password, $database, $prefix = NULL, $charset = 'UTF8') {
 		$file  = DIR_SYSTEM . 'database/' . $driver . '.php';
 		$class = 'Database' . preg_replace('/[^a-zA-Z0-9]/', '', $driver);
-
+		
 		if (file_exists($file)) {
 			include_once($file);
-
+			
 			$this->registry->set(str_replace('/', '_', $driver), new $class());
 		} else {
 			trigger_error('Error: Could not load database ' . $driver . '!');
-			exit();
+			exit();				
 		}
 	}
-
+	
 	public function config($config) {
 		$this->config->load($config);
 	}
-
+	
 	public function language($language) {
 		return $this->language->load($language);
 	}
-}
+
+	public function view($file,$params=array(),$return = FALSE)
+	{
+		
+	}
+} 
 ?>
--- system/library/log.php
+++ system/library/log.php
@@ -1,19 +1,62 @@
 <?php
 class Log {
+
 	private $filename;
+	
+	/**
+	 * Дескриптор файла лога. Инциализируется в момент создания объекта Log,
+	 * Очищается в момент удаления
+	 *
+	 * @var object
+	 **/
+	private $_oc_file_handle = null;
+
+	/**
+	 * Формтат доты сообщения
+	 *
+	 * @var string
+	 **/
+	private $_oc_dateformat = 'Y-m-d G:i:s';
 
 	public function __construct($filename) {
 		$this->filename = $filename;
 	}
 
+	public function __destruct()
+	{
+		if(!is_null($this->_oc_file_handle)){
+			fclose($this->_oc_file_handle);
+		}
+	}
+
+	public function set_filename($filename = '')
+	{
+		$this->filename = $filename;
+	}
+
+	public function get_filename()
+	{
+		return $this->filename;
+	}
+
+	public function get_log_filename()
+	{
+		return DIR_LOGS . $this->filename;
+	}
+
+	public function get_file_handle()
+	{
+		/*
+		* Если файл не создан, то создаем
+		*/
+		if(is_null($this->_oc_file_handle)){
+			$this->_oc_file_handle = fopen($this->get_log_filename(),'a+');
+		}
+		return $this->_oc_file_handle;
+	}
+	
 	public function write($message) {
-		$file = DIR_LOGS . $this->filename;
-
-		$handle = fopen($file, 'a+');
-
-		fwrite($handle, date('Y-m-d G:i:s') . ' - ' . $message . "\n");
-
-		fclose($handle);
+		fwrite($this->get_file_handle(), date($this->_oc_dateformat) . ' - ' . $message . "\n");			
 	}
 }
 ?>
